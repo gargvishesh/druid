14a653f7d04b94886f9021e52772a578681fb1ee
Suneet Saldanha <suneet.saldanha@imply.io>
2020-11-17 16:00:30 -0800

    Fix license header for imply extensions (#76)
    * Fix license header for imply extensions
    
    * arm64 packaging should use jdk8
    
    * maybe this time

--- extensions-imply/indexed-table-loader/src/main/java/io/imply/druid/segment/IndexedTableSegment.java
+++ extensions-imply/indexed-table-loader/src/main/java/io/imply/druid/segment/IndexedTableSegment.java
@@ -2,7 +2,9 @@
  * Copyright (c) Imply Data, Inc. All rights reserved.
  *
  * This software is the confidential and proprietary information
- * of Imply Data, Inc.
+ * of Imply Data, Inc. You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with Imply.
  */
 
 package io.imply.druid.segment;
