a36414e5745bb857897cdf432f53bc0d717461d8
Maytas Monsereenusorn <maytasm@apache.org>
2021-07-13 21:32:26 +0700

    fix checkstyle

--- extensions-imply/imply-manager-autoscaler/src/main/java/io/imply/druid/autoscaling/ImplyManagerAutoScaler.java
+++ extensions-imply/imply-manager-autoscaler/src/main/java/io/imply/druid/autoscaling/ImplyManagerAutoScaler.java
@@ -26,7 +26,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * This module permits the autoscaling of the workers in Imply Manager
