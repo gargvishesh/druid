b07624dc929b39af2b65e2ab818760a951899679
Paul Rogers <paul-rogers@users.noreply.github.com>
2022-01-20 16:34:41 -0800

    Remove dependence of Talaria on async metadata (#797)
    * Remove dependency of Talaria on async metadata
    
    Writes identity to the Talaria task. Uses the task
    info to validate the user instead of Async metadata.
    
    Adds the task "payload" to the Async details for Talaria.
    
    * Passed allong security exception
    
    * Depend on io.netty.
    
    Co-authored-by: Gian Merlino <gian@imply.io>

--- extensions-imply/imply-sql-async/src/main/java/io/imply/druid/sql/async/SqlAsyncResource.java
+++ extensions-imply/imply-sql-async/src/main/java/io/imply/druid/sql/async/SqlAsyncResource.java
@@ -12,11 +12,14 @@ package io.imply.druid.sql.async;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.google.inject.Key;
 import com.google.inject.name.Named;
 import io.imply.druid.sql.async.metadata.SqlAsyncMetadataManager;
 import io.imply.druid.sql.async.query.SqlAsyncQueryPool;
 import io.imply.druid.sql.async.result.SqlAsyncResultManager;
+import org.apache.druid.client.indexing.IndexingService;
 import org.apache.druid.client.indexing.IndexingServiceClient;
+import org.apache.druid.discovery.DruidLeaderClient;
 import org.apache.druid.guice.annotations.Json;
 import org.apache.druid.java.util.common.logger.Logger;
 import org.apache.druid.server.initialization.ServerConfig;
@@ -67,23 +70,23 @@ public class SqlAsyncResource
     // default in Druid. It will be bound only if the Talaria
     // extension is also loaded. To avoid spurious bindings,
     // we also check that Talaria is loaded.
-    IndexingServiceClient overlordClient;
+    IndexingServiceClient overlordClient = null;
+    DruidLeaderClient druidLeaderClient = null;
     try {
       if (injector == null) {
         // Testing case.
-        overlordClient = null;
       } else {
         // TODO: This is a fragile check.
         QueryMakerFactory qmFactory = injector.getInstance(QueryMakerFactory.class);
         if (qmFactory.getClass().getName().equals("io.imply.druid.talaria.sql.ImplyQueryMakerFactory")) {
           overlordClient = injector.getInstance(IndexingServiceClient.class);
-        } else {
-          overlordClient = null;
+          druidLeaderClient = injector.getInstance(
+              Key.get(DruidLeaderClient.class, IndexingService.class));
         }
       }
     }
     catch (Exception e) {
-      overlordClient = null;
+      // Ignore: not Talaria.
     }
     if (overlordClient == null) {
       LOG.debug("Broker-based async engine available.");
@@ -104,7 +107,8 @@ public class SqlAsyncResource
         asyncQueryReadRefreshConfig,
         clock,
         serverConfig,
-        overlordClient);
+        overlordClient,
+        druidLeaderClient);
     if (overlordClient == null) {
       this.driver = new BrokerAsyncQueryDriver(context);
     } else {
