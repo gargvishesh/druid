52391e4ce63f6f452648c333bbc8040f7d2235ab
Clint Wylie <cwylie@apache.org>
2022-05-11 23:59:40 -0700

    turn on broker parallel merge metrics for clarity emitter

--- extensions-imply/clarity-emitter/core/src/main/java/io/imply/clarity/metrics/ClarityGenericQueryMetrics.java
+++ extensions-imply/clarity-emitter/core/src/main/java/io/imply/clarity/metrics/ClarityGenericQueryMetrics.java
@@ -12,6 +12,9 @@ package io.imply.clarity.metrics;
 import io.imply.clarity.emitter.BaseClarityEmitterConfig;
 import org.apache.druid.query.DefaultQueryMetrics;
 import org.apache.druid.query.Query;
+import org.apache.druid.query.QueryMetrics;
+
+import java.util.concurrent.TimeUnit;
 
 public class ClarityGenericQueryMetrics<QueryType extends Query<?>> extends DefaultQueryMetrics<QueryType>
 {
@@ -71,5 +74,47 @@ public class ClarityGenericQueryMetrics<QueryType extends Query<?>> extends Defa
   {
     // Override superclass to avoid storing JSON-ified "context". Instead, pull out the specific dimensions we want.
     ClarityMetricsUtils.addContextDimensions(config, this::setDimension, builder::getDimension, query.getContext());
+  }
+
+  @Override
+  public void parallelMergeParallelism(int parallelism)
+  {
+    setDimension("mergeParallelism", parallelism);
+  }
+
+  @Override
+  public QueryMetrics<QueryType> reportParallelMergeParallelism(int parallelism)
+  {
+    return reportMetric("query/merge/parallelism", parallelism);
+  }
+
+  @Override
+  public QueryMetrics<QueryType> reportParallelMergeInputSequences(long numSequences)
+  {
+    return reportMetric("query/merge/sequences", numSequences);
+  }
+
+  @Override
+  public QueryMetrics<QueryType> reportParallelMergeInputRows(long numRows)
+  {
+    return reportMetric("query/merge/rows/input", numRows);
+  }
+
+  @Override
+  public QueryMetrics<QueryType> reportParallelMergeOutputRows(long numRows)
+  {
+    return reportMetric("query/merge/rows/output", numRows);
+  }
+
+  @Override
+  public QueryMetrics<QueryType> reportParallelMergeTaskCount(long numTasks)
+  {
+    return reportMetric("query/merge/tasks/count", numTasks);
+  }
+
+  @Override
+  public QueryMetrics<QueryType> reportParallelMergeTotalCpuTime(long timeNs)
+  {
+    return reportMetric("query/merge/cpu", TimeUnit.NANOSECONDS.toMillis(timeNs));
   }
 }
