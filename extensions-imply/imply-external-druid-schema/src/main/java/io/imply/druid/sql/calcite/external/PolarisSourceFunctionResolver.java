0aff1a68fb8826cdb27bc9dc49050e1711e7f58f
Abhishek Balaji Radhakrishnan <abhishek.rb19@gmail.com>
2023-01-25 16:59:54 +0530

    Wire in table function resolverm implementation.

---
+++ extensions-imply/imply-external-druid-schema/src/main/java/io/imply/druid/sql/calcite/external/PolarisSourceFunctionResolver.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Imply Data, Inc. All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Imply Data, Inc. You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with Imply.
+ */
+package io.imply.druid.sql.calcite.external;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.druid.java.util.common.IAE;
+import org.apache.druid.java.util.common.StringUtils;
+
+import io.imply.druid.sql.calcite.schema.tables.mapping.ExternalTableFunctionMapper;
+
+public class PolarisSourceFunctionResolver implements PolarisTableFunctionResolver {
+  private ExternalTableFunctionMapper tableFunctionMapper;
+  private ObjectMapper jsonMapper;
+
+  public PolarisSourceFunctionResolver(ExternalTableFunctionMapper m, ObjectMapper mapper) {
+    this.tableFunctionMapper = m;
+    this.jsonMapper = mapper;
+  }
+
+  @Override
+  public PolarisExternalTableSpec resolve(PolarisTableFunctionSpec fn) {
+    if (null == fn) {
+      throw new IAE("Table function spec cannot be null.");
+    }
+    try {
+      byte[] externalTableSpecBytes = tableFunctionMapper.getTableFunctionMapping(jsonMapper.writeValueAsBytes(fn));
+      return jsonMapper.readValue(externalTableSpecBytes, PolarisExternalTableSpec.class);
+    } catch (JsonProcessingException e) {
+      throw new IAE(StringUtils.format("Table function spec is malformed [%s]", e));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
