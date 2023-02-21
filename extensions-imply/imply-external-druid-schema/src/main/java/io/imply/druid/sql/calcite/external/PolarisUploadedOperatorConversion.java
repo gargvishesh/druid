8bf3f268be650d6b345e65b33856e88a24b30181
zachary sherman <zachjsh@gmail.com>
2023-02-21 10:45:40 -0800

    * fix

--- extensions-imply/imply-external-druid-schema/src/main/java/io/imply/druid/sql/calcite/external/PolarisUploadedOperatorConversion.java
+++ extensions-imply/imply-external-druid-schema/src/main/java/io/imply/druid/sql/calcite/external/PolarisUploadedOperatorConversion.java
@@ -9,24 +9,21 @@
 
 package io.imply.druid.sql.calcite.external;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import org.apache.druid.catalog.model.TableDefnRegistry;
-import org.apache.druid.guice.annotations.Json;
 import org.apache.druid.sql.calcite.external.CatalogExternalTableOperatorConversion;
 
 public class PolarisUploadedOperatorConversion extends CatalogExternalTableOperatorConversion
 {
   @Inject
   public PolarisUploadedOperatorConversion(
-      @Json final ObjectMapper jsonMapper,
       final PolarisTableFunctionResolver resolver,
       final TableDefnRegistry registry)
   {
     super(
         PolarisUploadedInputSourceDefn.PolarisUploadedFunctionSpec.FUNCTION_NAME,
         new PolarisUploadedInputSourceDefn(registry, resolver).adHocTableFn(),
-        jsonMapper
+        registry.jsonMapper()
     );
   }
 }
