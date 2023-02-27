a87f9d6e9574a16ebb3baec64701974bca630b7f
Abhishek Balaji Radhakrishnan <abhishek.rb19@gmail.com>
2023-02-27 09:25:57 -0800

    Add S3Connection function spec serde test.

---
+++ extensions-imply/imply-external-druid-schema/src/test/java/io/imply/druid/sql/calcite/external/PolarisS3ConnectionFunctionSpecTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Imply Data, Inc. All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Imply Data, Inc. You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with Imply.
+ */
+
+package io.imply.druid.sql.calcite.external;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PolarisS3ConnectionFunctionSpecTest
+{
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  @Test
+  public void testSerde() throws JsonProcessingException
+  {
+    PolarisS3ConnectionInputSourceDefn.PolarisS3ConnectionFunctionSpec spec =
+        new PolarisS3ConnectionInputSourceDefn.PolarisS3ConnectionFunctionSpec("sampleConn",
+                                                                               ImmutableList.of("foo"),
+                                                                               null,
+                                                                               ImmutableList.of("obj1"),
+                                                                               "foo/*.json");
+
+    PolarisTableFunctionSpec specSerde = MAPPER.readValue(
+        MAPPER.writeValueAsString(spec),
+        PolarisTableFunctionSpec.class);
+
+    Assert.assertEquals(spec, specSerde);
+  }
+
+  @Test
+  public void testEquals()
+  {
+    EqualsVerifier.simple().forClass(PolarisS3ConnectionInputSourceDefn.PolarisS3ConnectionFunctionSpec.class).verify();
+  }
+}
+
