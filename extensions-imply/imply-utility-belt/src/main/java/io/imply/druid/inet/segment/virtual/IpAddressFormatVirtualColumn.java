690da50ff5eae2661c53157de20ce2e41c139615
Clint Wylie <cwylie@apache.org>
2023-12-13 17:33:06 -0800

    fix build

--- extensions-imply/imply-utility-belt/src/main/java/io/imply/druid/inet/segment/virtual/IpAddressFormatVirtualColumn.java
+++ extensions-imply/imply-utility-belt/src/main/java/io/imply/druid/inet/segment/virtual/IpAddressFormatVirtualColumn.java
@@ -709,12 +709,6 @@ public class IpAddressFormatVirtualColumn implements VirtualColumn
       return new SimpleBitmapColumnIndex()
       {
         @Override
-        public double estimateSelectivity(int totalRows)
-        {
-          return delegate.getBitmapForValue(blob).size();
-        }
-
-        @Override
         public <T> T computeBitmapResult(BitmapResultFactory<T> bitmapResultFactory, boolean includeUnknown)
         {
           if (includeUnknown && blob != null) {
@@ -747,12 +741,6 @@ public class IpAddressFormatVirtualColumn implements VirtualColumn
       return new SimpleBitmapColumnIndex()
       {
         @Override
-        public double estimateSelectivity(int totalRows)
-        {
-          return Filters.estimateSelectivity(delegate.getBitmapsForValues(blobs).iterator(), totalRows);
-        }
-
-        @Override
         public <T> T computeBitmapResult(BitmapResultFactory<T> bitmapResultFactory, boolean includeUnknown)
         {
           if (includeUnknown && !containsNull) {
@@ -780,12 +768,6 @@ public class IpAddressFormatVirtualColumn implements VirtualColumn
     {
       return new SimpleBitmapColumnIndex()
       {
-        @Override
-        public double estimateSelectivity(int totalRows)
-        {
-          return Filters.estimateSelectivity(delegate.getBitmapsForPredicateFactory(matcherFactory, false).iterator(), totalRows);
-        }
-
         @Override
         public <T> T computeBitmapResult(BitmapResultFactory<T> bitmapResultFactory, boolean includeUnknown)
         {
