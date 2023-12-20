c395acd9a4920c60e181567b83c59c401d16c81b
Vishesh Garg <gargvishesh@gmail.com>
2023-12-20 14:52:38 +0530

    Initial commit

---
+++ compaction/src/main/java/org/hackathon/AWSClient.java
@@ -0,0 +1,50 @@
+package org.hackathon;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.DeleteObjectsResult;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AWSClient
+{
+  final String bucket;
+  final AmazonS3 s3Client;
+
+  final Integer maxListing;
+
+  public AWSClient(String bucket, Integer maxListing)
+  {
+    this.bucket = bucket;
+    s3Client = AmazonS3Client.builder().build();
+    this.maxListing = maxListing;
+  }
+
+  Set<String> getObjects(String prefix)
+  {
+    ListObjectsV2Request request = new ListObjectsV2Request()
+        .withBucketName(bucket)
+        .withPrefix(prefix)
+        .withMaxKeys(maxListing);
+
+    ListObjectsV2Result results = s3Client.listObjectsV2(request);
+    Set<String> objects = results.getObjectSummaries()
+                                 .stream()
+                                 .map(summary -> summary.getKey())
+                                 .collect(Collectors.toSet());
+    return objects;
+  }
+
+  void deleteObjects(Set<String> keys)
+  {
+    DeleteObjectsRequest request = new DeleteObjectsRequest(bucket);
+
+    request.setKeys(keys.stream().map(key -> new DeleteObjectsRequest.KeyVersion(key)).collect(Collectors.toList()));
+
+    DeleteObjectsResult results = s3Client.deleteObjects(request);
+  }
+}
