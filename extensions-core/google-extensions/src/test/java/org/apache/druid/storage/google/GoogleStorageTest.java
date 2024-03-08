09873eccacbbc9cdb9ba1afa15432fc6601ab671
Vishesh Garg <gargvishesh@gmail.com>
2024-03-08 14:51:06 +0530

    Add tests and other minor changes

--- extensions-core/google-extensions/src/test/java/org/apache/druid/storage/google/GoogleStorageTest.java
+++ extensions-core/google-extensions/src/test/java/org/apache/druid/storage/google/GoogleStorageTest.java
@@ -38,6 +38,7 @@ import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class GoogleStorageTest
@@ -157,7 +158,7 @@ public class GoogleStorageTest
   }
 
   @Test
-  public void testGetMetadata() throws IOException
+  public void testGetMetadataMatch() throws IOException
   {
     EasyMock.expect(mockStorage.get(
         EasyMock.eq(BUCKET),
@@ -173,7 +174,33 @@ public class GoogleStorageTest
     EasyMock.replay(mockStorage, blob);
 
     GoogleStorageObjectMetadata objectMetadata = googleStorage.getMetadata(BUCKET, PATH);
-    assertEquals(objectMetadata, new GoogleStorageObjectMetadata(BUCKET, PATH, SIZE, UPDATE_TIME.toEpochSecond()));
+    assertEquals(
+        objectMetadata,
+        new GoogleStorageObjectMetadata(BUCKET, PATH, SIZE, UPDATE_TIME.toEpochSecond() * 1000)
+    );
+
+  }
+  @Test
+  public void testGetMetadataMismatch() throws IOException
+  {
+    EasyMock.expect(mockStorage.get(
+        EasyMock.eq(BUCKET),
+        EasyMock.eq(PATH),
+        EasyMock.anyObject(Storage.BlobGetOption.class)
+    )).andReturn(blob);
+
+    EasyMock.expect(blob.getBucket()).andReturn(BUCKET);
+    EasyMock.expect(blob.getName()).andReturn(PATH);
+    EasyMock.expect(blob.getSize()).andReturn(SIZE);
+    EasyMock.expect(blob.getUpdateTimeOffsetDateTime()).andReturn(UPDATE_TIME);
+
+    EasyMock.replay(mockStorage, blob);
+
+    GoogleStorageObjectMetadata objectMetadata = googleStorage.getMetadata(BUCKET, PATH);
+    assertNotEquals(
+        objectMetadata,
+        new GoogleStorageObjectMetadata(BUCKET, PATH, SIZE, UPDATE_TIME.toEpochSecond())
+    );
 
   }
 
@@ -276,13 +303,13 @@ public class GoogleStorageTest
         bucket1,
         path1,
         size1,
-        updateTime1.toEpochSecond()
+        updateTime1.toEpochSecond() * 1000
     );
     GoogleStorageObjectMetadata objectMetadata2 = new GoogleStorageObjectMetadata(
         bucket2,
         path2,
         size2,
-        updateTime2.toEpochSecond()
+        updateTime2.toEpochSecond() * 1000
     );
 
     GoogleStorageObjectPage objectPage = googleStorage.list(BUCKET, PATH, null, null);
